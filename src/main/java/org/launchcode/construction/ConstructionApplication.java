d6a5feb1db5fd50538a6bd0e69da4c6797e935b6
Blake Mills <welzie@gmail.com>
2020-03-28 13:06:58 -0500

    changes to make this api work with a front end app running locally. prevent cors errors

--- src/main/java/org/launchcode/construction/ConstructionApplication.java
+++ src/main/java/org/launchcode/construction/ConstructionApplication.java
@@ -2,12 +2,25 @@ package org.launchcode.construction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConstructionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConstructionApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/items").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
